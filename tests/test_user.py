d3ed86b8b79f0cd6b0d2ac873a30cd5cb7fc4895
electron111111 <khomich.yan@gmail.com>
2022-06-01 23:11:19 +0300

    Rename variables

--- tests/test_user.py
+++ tests/test_user.py
@@ -29,40 +29,40 @@ def test_user():
 
     params = {"username": "Johnny", "password": "Johnny228"}
 
-    post_user = User().post_user(create_payload)
-    post_user.should_have_status_code(200)
-    post_user.should_have_body_field("code", 200)
-    post_user.should_have_body_field("message", str(create_payload["id"]))
+    create_user = User().post_user(create_payload)
+    create_user.should_have_status_code(200)
+    create_user.should_have_body_field("code", 200)
+    create_user.should_have_body_field("message", str(create_payload["id"]))
 
-    get_login_user = User().get_login_user(params)
-    get_login_user.should_have_status_code(200)
-    get_login_user.should_have_body_field("code", 200)
-    get_login_user.does_str_in_value("message", "logged in user session:")
+    login_user = User().get_login_user(params)
+    login_user.should_have_status_code(200)
+    login_user.should_have_body_field("code", 200)
+    login_user.does_str_in_value("message", "logged in user session:")
 
-    put_user = User().put_user(create_payload["username"], update_payload)
-    put_user.should_have_status_code(200)
-    put_user.should_have_body_field("code", 200)
-    put_user.should_have_body_field("message", str(update_payload["id"]))
+    update_user = User().put_user(create_payload["username"], update_payload)
+    update_user.should_have_status_code(200)
+    update_user.should_have_body_field("code", 200)
+    update_user.should_have_body_field("message", str(update_payload["id"]))
 
-    get_user = User().get_user_by_username(update_payload["username"])
-    get_user.should_have_status_code(200)
-    get_user.should_have_body_field("id", update_payload["id"])
-    get_user.should_have_body_field("username", update_payload["username"])
-    get_user.should_have_body_field("firstName", update_payload["firstName"])
-    get_user.should_have_body_field("lastName", update_payload["lastName"])
-    get_user.should_have_body_field("email", update_payload["email"])
-    get_user.should_have_body_field("password", update_payload["password"])
-    get_user.should_have_body_field("phone", update_payload["phone"])
+    get_user_by_username = User().get_user_by_username(update_payload["username"])
+    get_user_by_username.should_have_status_code(200)
+    get_user_by_username.should_have_body_field("id", update_payload["id"])
+    get_user_by_username.should_have_body_field("username", update_payload["username"])
+    get_user_by_username.should_have_body_field("firstName", update_payload["firstName"])
+    get_user_by_username.should_have_body_field("lastName", update_payload["lastName"])
+    get_user_by_username.should_have_body_field("email", update_payload["email"])
+    get_user_by_username.should_have_body_field("password", update_payload["password"])
+    get_user_by_username.should_have_body_field("phone", update_payload["phone"])
 
     delete_user = User().delete_user(update_payload["username"])
     delete_user.should_have_status_code(200)
     delete_user.should_have_body_field("code", 200)
     delete_user.should_have_body_field("message", str(update_payload["username"]))
 
-    get_logout_user = User().get_logout_user()
-    get_logout_user.should_have_status_code(200)
-    get_logout_user.should_have_body_field("code", 200)
-    get_logout_user.should_have_body_field("message", "ok")
+    logout_user = User().get_logout_user()
+    logout_user.should_have_status_code(200)
+    logout_user.should_have_body_field("code", 200)
+    logout_user.should_have_body_field("message", "ok")
 
 
 def test_create_list_of_users():
@@ -90,23 +90,23 @@ def test_create_list_of_users():
         }
     ]
 
-    post_users_with_array = User().post_list_of_users_with_array(payload)
-    post_users_with_array.should_have_status_code(200)
-    post_users_with_array.should_have_body_field("code", 200)
-    post_users_with_array.should_have_body_field("message", "ok")
+    create_list_of_users_with_array = User().post_list_of_users_with_array(payload)
+    create_list_of_users_with_array.should_have_status_code(200)
+    create_list_of_users_with_array.should_have_body_field("code", 200)
+    create_list_of_users_with_array.should_have_body_field("message", "ok")
 
-    post_users_with_list = User().post_list_of_users_with_list(payload)
-    post_users_with_list.should_have_status_code(200)
-    post_users_with_list.should_have_body_field("code", 200)
-    post_users_with_list.should_have_body_field("message", "ok")
+    create_list_of_users_with_list = User().post_list_of_users_with_list(payload)
+    create_list_of_users_with_list.should_have_status_code(200)
+    create_list_of_users_with_list.should_have_body_field("code", 200)
+    create_list_of_users_with_list.should_have_body_field("message", "ok")
 
 
 def test_get_user_with_not_existing_username():
 
-    get_user = User().get_user_by_username("21312312312qwe213")
-    get_user.should_have_status_code(404)
-    get_user.should_have_body_field("code", 1)
-    get_user.should_have_body_field("message", "User not found")
+    get_user_by_username = User().get_user_by_username("21312312312qwe213")
+    get_user_by_username.should_have_status_code(404)
+    get_user_by_username.should_have_body_field("code", 1)
+    get_user_by_username.should_have_body_field("message", "User not found")
 
 
 def test_delete_user_with_not_existing_username():
