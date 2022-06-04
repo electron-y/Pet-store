d4c3d4ab684489a92a3d5bba738be141076f629f
electron111111 <khomich.yan@gmail.com>
2022-06-04 21:51:54 +0300

    has deleted unnecessary assertable responses

--- src/assertable_response.py
+++ src/assertable_response.py
@@ -8,7 +8,6 @@ class AssertableResponse:
         self.condition = Conditions(self.response.json())
 
     def should_have_status_code(self, status_code):
-        # print(self.response.status_code, "|", status_code)
         assert self.response.status_code == status_code, f'{self.response.status_code} | {status_code}: \n ' \
                                                          f'{self.condition.show_response_body()}'
 
@@ -17,19 +16,6 @@ class AssertableResponse:
 
         if field_value:
             assert self.condition.field_have_value(field_name, field_value) is True
-
-    '''def should_not_have_body_field(self, field_name, field_value=False):
-        if field_value:
-            assert self.condition.field_have_value(field_name, field_value) is False
-        else:
-            assert self.condition.have_field(field_name) is False'''
-
-    '''def show_response_body(self):
-        # print(self.condition.show_response_body())
-        return self.condition.show_response_body()'''
-
-    '''def get_value(self, key):
-        return self.condition.get_params_request(key)'''
 
     def does_str_in_value(self, field_name, field_value):
         assert self.condition.does_str_in_value(field_name, field_value)
