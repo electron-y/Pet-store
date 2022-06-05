c8cb8db33476f87339b4686462e3f67ef458857f
electron111111 <khomich.yan@gmail.com>
2022-06-05 21:32:47 +0300

    update name of variable

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -3,7 +3,7 @@ import pytest
 from src.wrappers.pet import Pet
 
 
-update_payload = {
+payload_update_pet = {
         "id": 1212,
         "category": {
             "id": 2121,
@@ -25,23 +25,23 @@ update_payload = {
 
 def test_update_existing_pet_and_get(add_pet, delete_pet):
 
-    update_existing_pet = Pet().put_existing_pet(update_payload)
+    update_existing_pet = Pet().put_existing_pet(payload_update_pet)
     update_existing_pet.should_have_status_code(200)
-    update_existing_pet.should_have_body_field("id", update_payload["id"])
-    update_existing_pet.should_have_body_field("category", update_payload["category"])
-    update_existing_pet.should_have_body_field("name", update_payload["name"])
-    update_existing_pet.should_have_body_field("status", update_payload["status"])
+    update_existing_pet.should_have_body_field("id", payload_update_pet["id"])
+    update_existing_pet.should_have_body_field("category", payload_update_pet["category"])
+    update_existing_pet.should_have_body_field("name", payload_update_pet["name"])
+    update_existing_pet.should_have_body_field("status", payload_update_pet["status"])
 
-    get_pet_by_id = Pet().get_pet_by_id(update_payload["id"])
+    get_pet_by_id = Pet().get_pet_by_id(payload_update_pet["id"])
     get_pet_by_id.should_have_status_code(200)
-    get_pet_by_id.should_have_body_field("id", update_payload["id"])
-    get_pet_by_id.should_have_body_field("category", update_payload["category"])
-    get_pet_by_id.should_have_body_field("name", update_payload["name"])
-    get_pet_by_id.should_have_body_field("status", update_payload["status"])
+    get_pet_by_id.should_have_body_field("id", payload_update_pet["id"])
+    get_pet_by_id.should_have_body_field("category", payload_update_pet["category"])
+    get_pet_by_id.should_have_body_field("name", payload_update_pet["name"])
+    get_pet_by_id.should_have_body_field("status", payload_update_pet["status"])
 
 
 @pytest.mark.parametrize("status", ['available', 'pending', 'sold'])
-def test_pet_status(status):
+def test_get_pets_by_status(status):
 
     params = {"status": status}
 
@@ -53,12 +53,12 @@ def test_update_existing_pet_with_new_form_data_and_get_pet(add_pet, delete_pet)
 
     params = {"name": "Bonny1", "status": "sold"}
 
-    update_existing_pet_with_new_form_data = Pet().post_existing_pet_with_form_data(update_payload["id"], params)
+    update_existing_pet_with_new_form_data = Pet().post_existing_pet_with_form_data(payload_update_pet["id"], params)
     update_existing_pet_with_new_form_data.should_have_status_code(200)
     update_existing_pet_with_new_form_data.should_have_body_field("code", 200)
-    update_existing_pet_with_new_form_data.should_have_body_field("message", str(update_payload["id"]))
+    update_existing_pet_with_new_form_data.should_have_body_field("message", str(payload_update_pet["id"]))
 
-    get_pet_by_id = Pet().get_pet_by_id(update_payload["id"])
+    get_pet_by_id = Pet().get_pet_by_id(payload_update_pet["id"])
     get_pet_by_id.should_have_status_code(200)
     get_pet_by_id.should_have_body_field("name", params["name"])
     get_pet_by_id.should_have_body_field("status", params["status"])
@@ -68,7 +68,7 @@ def test_upload_image_to_existing_pet(add_pet, delete_pet):
 
     file = {'file': ('image_png_1MB.png', open('/Users/y.khomych/Desktop/test files/image_png_1MB.png', 'rb'), 'image/png')}
 
-    upload_image_to_existing_pet = Pet().post_image_to_pet(update_payload["id"], file)
+    upload_image_to_existing_pet = Pet().post_image_to_pet(payload_update_pet["id"], file)
     upload_image_to_existing_pet.should_have_status_code(200)
     upload_image_to_existing_pet.should_have_body_field("code", 200)
     upload_image_to_existing_pet.should_have_body_field("message",
