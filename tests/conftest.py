3eb9bd5a3d835a054c210518e4b8ced5fb779b83
electron111111 <khomich.yan@gmail.com>
2022-06-12 13:44:39 +0300

    has removed fixtures for moving

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,83 +1,6 @@
 import pytest
 
-from src.wrappers.pet import Pet
-from src.wrappers.user import User
-from src.wrappers.store import Store
 from src.wrappers.authentication import Authentication
-
-
-payload_create_pet = {
-        "id": 1212,
-        "category": {
-            "id": 1,
-            "name": "Cat"
-        },
-        "name": "Mikasa",
-        "photoUrls": [
-            "string"
-        ],
-        "tags": [
-            {
-                "id": 0,
-                "name": "string"
-            }
-        ],
-        "status": "available"
-    }
-
-
-@pytest.fixture()
-def add_pet():
-
-    add_new_pet = Pet().post_new_pet(payload_create_pet)
-    add_new_pet.should_have_status_code(200)
-    add_new_pet.should_have_body_field("id", payload_create_pet["id"])
-    add_new_pet.should_have_body_field("category", payload_create_pet["category"])
-    add_new_pet.should_have_body_field("name", payload_create_pet["name"])
-    add_new_pet.should_have_body_field("status", payload_create_pet["status"])
-
-
-@pytest.fixture()
-def delete_pet():
-
-    yield
-
-    delete_pet = Pet().delete_pet(payload_create_pet["id"])
-    delete_pet.should_have_status_code(200)
-    delete_pet.should_have_body_field("code", 200)
-    delete_pet.should_have_body_field("message", str(payload_create_pet["id"]))
-
-
-payload_create_user = {
-        "id": 112233,
-        "username": "Johnny",
-        "firstName": "John",
-        "lastName": "Smith",
-        "email": "john228@gmail.com",
-        "password": "Johnny228",
-        "phone": "380777777777",
-        "userStatus": 0
-    }
-
-
-@pytest.fixture()
-def create_user():
-
-    create_user = User().post_user(payload_create_user)
-    create_user.should_have_status_code(200)
-    create_user.should_have_body_field("code", 200)
-    create_user.should_have_body_field("message", str(payload_create_user["id"]))
-
-
-@pytest.fixture()
-def delete_user():
-
-    yield
-
-    delete_user = User().delete_user(payload_create_user["username"])
-    delete_user.should_have_status_code(200)
-    delete_user.should_have_body_field("code", 200)
-    delete_user.should_have_body_field("message", str(payload_create_user["username"]))
 
 
 @pytest.fixture()
@@ -89,21 +12,3 @@ def login_user():
     login_user.should_have_status_code(200)
     login_user.should_have_body_field("code", 200)
     login_user.does_str_in_value("message", "logged in user session:")
-
-
-@pytest.fixture()
-def delete_order():
-    payload_create_order = {
-        "id": 123,
-        "petId": 1111,
-        "quantity": 1,
-        "shipDate": "2022-05-16T13:32:37.914+0000",
-        "status": "placed"
-    }
-
-    yield
-
-    delete_order_by_id = Store().delete_order(payload_create_order["id"])
-    delete_order_by_id.should_have_status_code(200)
-    delete_order_by_id.should_have_body_field("code", 200)
-    delete_order_by_id.should_have_body_field("message", str(payload_create_order["id"]))
