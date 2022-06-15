da6cee856246f7a21884df6103c89d5b4b1cd5a3
electron111111 <khomich.yan@gmail.com>
2022-06-15 21:06:56 +0300

    remove response,json from init function

--- src/assertable_response.py
+++ src/assertable_response.py
@@ -5,20 +5,19 @@ class AssertableResponse:
 
     def __init__(self, response):
         self.response = response
-        self.condition = Conditions(self.response.json())
+        self.condition = Conditions
 
     def should_have_status_code(self, status_code):
-        assert self.response.status_code == status_code, f'{self.response.status_code} | {status_code}: \n ' \
-                                                         f'{self.condition.show_response_body()}'
+        assert self.response.status_code == status_code, f'{self.response.status_code} | {status_code}'
 
     def should_have_body_field(self, field_name, field_value=False):
-        assert self.condition.have_field(field_name) is True
+        assert self.condition(self.response.json()).have_field(field_name) is True
 
         if field_value:
-            assert self.condition.field_have_value(field_name, field_value) is True
+            assert self.condition(self.response.json()).field_have_value(field_name, field_value) is True
 
     def does_str_in_value(self, field_name, field_value):
-        assert self.condition.does_str_in_value(field_name, field_value)
+        assert self.condition(self.response.json()).does_str_in_value(field_name, field_value)
 
     def get_value(self, key):
-        return self.condition.get_params_request(key)
+        return self.condition(self.response.json()).get_params_request(key)
